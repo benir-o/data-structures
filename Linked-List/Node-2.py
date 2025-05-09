78585b684dbe3a8cdf85014df3c7204ed6563e5d
benir-o <beniromenda@gmail.com>
2025-05-09 11:32:24 +0300

    Created a Node for practice purposes

--- Linked-List/Node-2.py
+++ Linked-List/Node-2.py
@@ -2,3 +2,9 @@ class Node:
     def __init__(self,data):
         self.data=data
         self.next=None # This is a pointer to the next node
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def append(self,data):
+        current_node=Node(data)
