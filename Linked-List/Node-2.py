a5f6cab858829c3c6b42db4e6b941ab03c1c9d1d
benir-o <beniromenda@gmail.com>
2025-05-10 20:21:24 +0300

    Implemented a python Linked list

--- Linked-List/Node-2.py
+++ Linked-List/Node-2.py
@@ -1,8 +1,34 @@
 class Node:
     def __init__(self,data):
         self.data=data
-        self.next=None # This is a pointer to the next node
+        self.next=None
 
-    def append(self,data):
+
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+    def append_node(self,data):
         new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+        else:
+            current_node=self.head
+            while current_node.next is not None:
+                current_node=current_node.next
+            current_node.next=new_node
+    def print_nodes(self):
+        while self.head.next is not None:
+            print(self.head.data,'-->',end=" ")
+            self.head=self.head.next
+        print(self.head.data)
+
+
+node_blocks=Linkedlist()
+node_blocks.append_node(10)
+node_blocks.append_node(20)
+node_blocks.append_node(30)
+node_blocks.append_node(40)
+node_blocks.print_nodes()
+
+
 
